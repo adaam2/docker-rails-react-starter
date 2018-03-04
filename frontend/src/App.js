7fb38631e85e6d70900c41a0fb4cd40eb37146c2
Adam Bull <adamjamesbull@googlemail.com>
2018-03-04 04:19:14 +0000

    Working fe, just not updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
