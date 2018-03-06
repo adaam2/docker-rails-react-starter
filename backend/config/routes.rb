e21957d395ad349c5cbaea85668529d0d4f7b00e
Adam Bull <adamjamesbull@googlemail.com>
2018-03-06 17:04:03 +0000

    Get db connection working in Rails app

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'api/status#index'
 
   namespace :api do
+    mount_devise_token_auth_for 'User', at: 'auth'
+
     get '/', to: 'status#index'
   end
 end
--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,4 +5,5 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+echo -e "\nRunning server"
 exec bundle exec "$@"
